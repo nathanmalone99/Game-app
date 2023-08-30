165b094a2db61b673a4cc27be315b89daf27c98c
S00188508 <S00188508@mail.itsligo.ie>
2023-08-30 21:10:42 +0100

    added some buttons for routing

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+
+//mongodb+srv://nathanmalone99:test1234@cluster0.nxh5bqi.mongodb.net/?retryWrites=true&w=majority
+
  mongoose.connect('mongodb://127.0.0.1:27017/osd', {
     useNewUrlParser: true,
     useUnifiedTopology: true
